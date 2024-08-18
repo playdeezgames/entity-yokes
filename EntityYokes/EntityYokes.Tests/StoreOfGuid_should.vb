cfbb25bfad1c4bdc88c01831c19c8921655adc48
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-08-18 05:06:01 -0500

    guid as an identifier works.

---
+++ EntityYokes/EntityYokes.Tests/StoreOfGuid_should.vb
@@ -0,0 +1,7 @@
+﻿Public Class StoreOfGuid_should
+    Inherits Store_should(Of Guid)
+
+    Protected Overrides Function NextIdentifier() As Guid
+        Return Guid.NewGuid
+    End Function
+End Class
