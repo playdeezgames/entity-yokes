c34ba35f0dbcfb4d21cf1b068d59dc7b78d04ac8
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-08-19 05:25:35 -0500

    entity of guid should

---
+++ EntityYokes/EntityYokes.Tests/EntityOfGuid_should.vb
@@ -0,0 +1,7 @@
+﻿Public Class EntityOfGuid_should
+    Inherits Entity_should(Of Guid)
+
+    Protected Overrides Function NextEntityIdentifier() As Guid
+        Return Guid.NewGuid
+    End Function
+End Class
